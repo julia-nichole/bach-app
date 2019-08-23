bc496777c74753ede56419edf77ae7e552099487
Julia Galan <juliag@Julias-MacBook-Pro.local>
2019-08-23 14:45:35 -0500

    two

--- models/user.js
+++ models/user.js
@@ -2,10 +2,8 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema
 //google id 
 const userSchema = new Schema ({
-    name: String,
-    email:{type:String, required: true, },
-    password: String,
-},{
+    googleId: String
+}, {
     timestamps: true
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var apiCtrl = require('../controllers/api/api')
+var apiCtrl = require('../controllers/api/api');
+var passport = require('passport');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/users');
+});
+// oauth login route 
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+//oauth callback 
+router.get('/oauth2callback',passport.authenticate(
+  'google',
+  {
+    successRedirect : '/users',
+    failureRedirect : '/users'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
-router.post('./search',apiCtrl.yelpFilter);
+
+// router.post('./search',apiCtrl.yelpFilter);
 module.exports = router;
